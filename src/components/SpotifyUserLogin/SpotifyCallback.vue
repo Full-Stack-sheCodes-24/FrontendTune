9ceb0942c50eac033dba92f57b5cce16a5c69cb1
yuunster <kayuuha@gmail.com>
2024-10-25 00:51:57 -0700

    Created spotify login button and authentication flow layout

---
+++ src/components/SpotifyUserLogin/SpotifyCallback.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Loading...</h1>
+    </div>
+</template>
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+onBeforeMount(() => {
+    const code = route.query.code;
+    const state = route.query.state;
+    if (sessionStorage.getItem('spotify_state') === state) {
+        console.log(code);
+        //send code to our backend
+        router.push({ name: 'Home' });
+    }
+})
+
+</script>
