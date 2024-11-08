8398d347ab55353f3c0af265aa9b8dcc40443667
Justin <kayuuha@gmail.com>
2024-11-08 14:37:59 -0800

    Attach jwt token to spotifySearchClient request

--- src/Shared/Clients/SpotifySearchClient.ts
+++ src/Shared/Clients/SpotifySearchClient.ts
@@ -4,9 +4,16 @@ import type { SpotifySearchResponse } from './SpotifySearchResponse';
 
 export class SpotifySearchClient {
     async execute(request : SpotifySearchRequest) : Promise<SpotifySearchResponse> {
+        const userState = localStorage.getItem('user_state');
+        const parsedUserState = JSON.parse(userState!);
+        const token = parsedUserState.jwtToken;
+
         const client = axios.create({
             baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
-            params: request
+            params: request,
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
         });
 
         const response = await client.get('/Spotify/search');
