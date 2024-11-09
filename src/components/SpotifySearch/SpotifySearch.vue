23462b6997f6aa1926263d26a886c615fd5e5f18
erikajledesma <erika.jasmine06@gmail.com>
2024-11-08 21:05:27 -0800

    display user selected song

--- src/components/SpotifySearch/SpotifySearch.vue
+++ src/components/SpotifySearch/SpotifySearch.vue
@@ -1,9 +1,10 @@
 <template>
+    <p v-if="isSelected">{{ selectedSong }}</p>
     <div>
         <input type="text" v-model="inputText" placeholder="Type in song name" id="searchInput">
         <button @click="search(inputText)">Search</button>
         <div v-for="track in searchResults">
-            <button>Select</button>
+            <button @click="selectSong(track.name)">Select</button>
             {{ track.name }}
         </div>
     </div>
@@ -12,11 +13,13 @@
 import { ref } from 'vue';
 import type { Track } from '../../Shared/Models/Track';
 import { SpotifySearchMapper } from './Mappers/SpotifySearchMapper';
-import { SpotifySearchClient } from '../../Shared/Clients/SpotifySearchClient';
+import { SpotifySearchClient } from '../../Shared/Clients/SpotifySearchClient'; 
 
 const searchResults = ref([] as Track[]);
 const inputText = ref();
 const mapper = new SpotifySearchMapper();
+const selectedSong = ref('');
+const isSelected = ref(false);
 
 function search(query : string) {
     const client = new SpotifySearchClient();
@@ -24,5 +27,10 @@ function search(query : string) {
     client.execute({ query }).then(response => {
         searchResults.value = mapper.searchResponseToTracks(response);
     });
+}
+
+function selectSong(trackName : string){
+    selectedSong.value = trackName;
+    isSelected.value = true;
 }
 </script>
