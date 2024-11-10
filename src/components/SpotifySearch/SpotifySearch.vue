729802cf138cf72189f06856ce3ff86cde006855
anjanetttee <anjanette.auwlia@gmail.com>
2024-11-10 13:02:28 -0800

    Scrollable results + styling

--- src/components/SpotifySearch/SpotifySearch.vue
+++ src/components/SpotifySearch/SpotifySearch.vue
@@ -22,9 +22,13 @@
     <div>
         <input type="text" v-model="inputText" placeholder="Type in song name" id="searchInput">
         <button @click="search(inputText)">Search</button>
-        <div v-for="track in searchResults">
-            <button @click="selectSong(track)">Select</button>
+        <div class = "spotify-search-results">
+            <div v-for="track in searchResults"
+                :key = "track.id"
+                @click="selectSong(track)"
+                class = "track-item">
             {{ track.name }}
+        </div>
         </div>
     </div>
 </template>
