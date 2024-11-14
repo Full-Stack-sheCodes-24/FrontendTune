420091744644c02e4efd7ae4d51c306c07553eca
Aizhenii <141988641+Aizhenii@users.noreply.github.com>
2024-11-14 11:05:54 -0800

    Added days and hardcoded image on the calendar item card.

--- src/components/Calender/CalenderItem.vue
+++ src/components/Calender/CalenderItem.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="calender-item card clickable">
+        
+        <p class="days-on-calendar">{{ currentDay }}</p>    
+        <img src="https://spiralcute.com/characters/img/characters/thumb_chiikawa.jpg"/>
     </div>
 </template>
+
 <script setup lang="ts">
+const props = defineProps<{
+    currentDay: number;
+    albumCoverCalendar: string | undefined;
+}>();
 </script>
