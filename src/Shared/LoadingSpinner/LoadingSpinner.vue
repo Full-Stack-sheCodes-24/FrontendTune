4575a3d69d90d8ab3a634e8391b6e3db1e24ed4a
Justin <kayuuha@gmail.com>
2024-11-15 19:44:29 -0800

    added loading spinner

---
+++ src/Shared/LoadingSpinner/LoadingSpinner.vue
@@ -0,0 +1,12 @@
+<style>@import'./LoadingSpinner.css';</style>
+<template>
+    <div class="loading-spinner-container">
+        <img class="loading-spinner-main" src="@/assets/loading_spinner_main.png">
+        <div class="loading-spinner-accents">
+            <img class="loading-spinner-accent1" src="@/assets/loading_spinner_accent1.png">
+            <img class="loading-spinner-accent2" src="@/assets/loading_spinner_accent2.png">
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
