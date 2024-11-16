eb6e0058f935c1d563815400fa0de47aaa3db414
Mohraiel Matta <mohraielmatta@gmail.com>
2024-11-15 20:00:30 -0800

    click on logo to display preview of the track

--- src/components/EntryItem/EntryItem.vue
+++ src/components/EntryItem/EntryItem.vue
@@ -2,11 +2,22 @@
 <template>
     <div class="entry-container card clickable">
         <h1 v-text="entry.text"></h1>
-        <p v-text="entry.track.name"></p>
+        <p>{{ entry.track.name }}  
+        <div class="favicon-container" @click="togglePlayback">
+            <img src="/favicon.ico" alt="Play Icon" class="favicon-icon" />
+        </div>
+        </p>
+        <div class="music-play" v-if="showPlayback">
+            <audio controls autoplay loop :src="entry.track.preview_url || undefined">
+                Your browser does not support the audio element.
+            </audio>
+        </div>
+        <p v-if="!entry.track.preview_url">No preview available for this track</p>
         <p v-text="entry.date.toLocaleString()"></p>
     </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
 import type { PropType } from 'vue';
 import type { Entry } from '@/Shared/Models/Entry';
 
@@ -15,5 +26,10 @@ defineProps({
         type: Object as PropType<Entry>,
         required: true
     }
-})
+});
+const showPlayback = ref(false);
+
+function togglePlayback() {
+    showPlayback.value = !showPlayback.value;
+}
 </script>
