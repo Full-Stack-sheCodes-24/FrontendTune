685e0300cba7492450903a4274d9489838eb4e91
Aizhenii <141988641+Aizhenii@users.noreply.github.com>
2024-11-15 18:06:29 -0800

    Added control to switch month on calendar

--- src/components/Calender/Calender.vue
+++ src/components/Calender/Calender.vue
@@ -1,8 +1,19 @@
 <style>@import'./Calender.css';</style>
 <template>
     <div class="calender-container card">
-        <h1 v-text="month"></h1>
-        <div class="calender-item-container">
+        <div class="calendar-title">
+            <button class="calendar-arrow" @click="nextPreviousMonth(-1)">
+                <i class="material-symbols-outlined">chevron_left</i>
+            </button>
+            <h1 v-text="month"></h1>
+            <!-- <p v-text="year"></p> -->
+            <button class="calendar-arrow" @click="nextPreviousMonth(1)">
+                <i class="material-symbols-outlined">chevron_right</i>
+            </button>
+
+        </div>
+        <p v-text="year"></p>
+        <div class="calender-item-container">    
             <CalenderItem v-for="day in days"
                 :currentDay="day"
                 :albumCoverCalendar=undefined
@@ -10,22 +21,43 @@
         </div>
     </div>
 </template>
+
 <script setup lang="ts">
 import CalenderItem from './CalenderItem.vue';
+import { ref } from 'vue';
+
+const today = new Date();
+let currentMonth = today.getMonth(); // 0 for January, 11 for December
+let currentYear = today.getFullYear();
 
 const monthNames: string[] = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
 
-const days = getDaysInCurrentMonth();
-const month = monthNames[new Date().getMonth()];
+const days = ref(getDaysInCurrentMonth());
+const month = ref(monthNames[currentMonth]);
+const year = ref(currentYear);
+
+const nextPreviousMonth = (value: number) => {
+    currentMonth += value;
+
+    if (currentMonth == 12) {
+        currentYear += 1;
+        currentMonth = 0;
+    }
+    else if (currentMonth == -1) {
+        currentYear -= 1;
+        currentMonth = 11;
+    }
+
+    month.value = monthNames[currentMonth];
+    days.value = getDaysInCurrentMonth();
+    year.value = currentYear;
+    // console.log("next month");
+}
 
 function getDaysInCurrentMonth(): number {
-    const today = new Date();
-    const currentMonth = today.getMonth(); // 0 for January, 11 for December
-    const currentYear = today.getFullYear();
-
     // Create a new date for the first day of the next month
     const firstDayNextMonth = new Date(currentYear, currentMonth + 1, 1);
     
