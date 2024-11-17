d7f5e784ebfbbf91040e3302fa929aba97cc4fa0
anjanetttee <anjanette.auwlia@gmail.com>
2024-11-17 00:58:46 -0800

    pop out description

--- src/components/AboutUs/AboutUs.vue
+++ src/components/AboutUs/AboutUs.vue
@@ -19,15 +19,15 @@
         --quantity: 9;
     ">
     <div class="list">
-            <div class="item" style ="--position: 1"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 2"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 3"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 4"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 5"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 6"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 7"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 8"><img src ="/src/assets/jenjen.jpg"></div>
-            <div class="item" style ="--position: 9"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 1"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 2"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 3"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 4"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 5"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 6"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 7"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 8"><img src ="/src/assets/pfp.jpg"></div>
+            <div class="item" style ="--position: 9"><img src ="/src/assets/pfp.jpg"></div>
         </div>
     </div>
 </template>
