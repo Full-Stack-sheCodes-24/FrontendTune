e21b9485a9f53c3d2fb0609e957205ec0a69c9d5
anjanetttee <anjanette.auwlia@gmail.com>
2024-11-16 18:10:51 -0800

    descriptions

--- src/components/AboutUs/AboutUs.vue
+++ src/components/AboutUs/AboutUs.vue
@@ -1,51 +1,35 @@
 <style>@import'./AboutUs.css';</style>
 <template>
-    <div class = "about-title">
-        <p>About Us</p>
-        <!-- <img src ="/src/assets/logo.png"> -->
-    </div>
-
     <div class = "app-desc">
-       <p>Moodz is a full stack web application created for Fall 2024 SheCodes Semester Long Project. 
-        It is a journalling application that enables a user to add a song that complements their journal entry.
-        Users login to the application through Spotify and will be directed to the home page. 
-        The user will be able to edit their profile picture, bio, and add their birthdays. 
-    </p>
+        <p class = "desc-title">Welcome to Moodz!</p>
+        <!-- <img class="logo-design" src="/public/favicon.ico"> -->
     </div>
-    <br>
-    <div class = "tech-stack">
-        <p class ="title">Tech Stack</p>
-        <p>Frontend</p>
-        <img src ="/src/assets/Vue.png" height = 100px>
-        <img src ="/src/assets/figma.png" height = 100px>
-        <img src ="/src/assets/canva.png" height = 100px>
-        <p>Backend</p>
-        <img src ="/src/assets/csharp.png" height = 100px>
-        <img src ="/src/assets/mongodb.png" height = 100px>
-        <p>Agile Dev</p>
-        <img src ="/src/assets/github.png" height = 100px>
-        <img src ="/src/assets/discord.png" height = 100px>
-        <img src ="/src/assets/devops.png" height = 100px>
+    <div class = "about-title">
+        <!-- <p>About Us</p> -->
+        <p class = "desc"> Moodz is a journalling application that enables a user to add a song that complements their journal entry.
+            Users login to the application through Spotify and will be directed to the home page. 
+            The user will be able to edit their profile picture, bio, and add their birthdays.Moodz is a journalling application that enables a user to add a song that complements their journal entry.
+            Users login to the application through Spotify and will be directed to the home page. 
+            The user will be able to edit their profile picture, bio, and add their birthdays</p>
     </div>
-    <br>
     <div class = "our-team">
         <p>Our Team</p>
+    </div>
     <div class="slider" style ="
         --width: 200px;
         --height: 500px;
         --quantity: 9;
     ">
-        <div class="list">
-            <div class="item" style ="--position: 1"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 2"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 3"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 4"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 5"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 6"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 7"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 8"><img src ="/src/assets/pfp.jpg"></div>
-            <div class="item" style ="--position: 9"><img src ="/src/assets/pfp.jpg"></div>
+    <div class="list">
+            <div class="item" style ="--position: 1"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 2"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 3"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 4"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 5"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 6"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 7"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 8"><img src ="/src/assets/jenjen.jpg"></div>
+            <div class="item" style ="--position: 9"><img src ="/src/assets/jenjen.jpg"></div>
         </div>
     </div>
-</div>
 </template>
